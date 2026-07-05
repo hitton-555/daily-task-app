8bfeb0dc3674e2ff43faa95de137470219d22562
hitton-555 <299717495+hitton-555@users.noreply.github.com>
2026-07-05 17:21:28 +0900

    初回コミット: 今日のタスク管理アプリ（PWA）

---
+++ sw.js
@@ -0,0 +1,28 @@
+// オフラインでもホーム画面アプリとして起動できるようにする Service Worker
+const CACHE = "daily-tasks-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg",
+  "./apple-touch-icon.png", "./apple-touch-icon-180.png",
+  "./apple-touch-icon-192.png", "./apple-touch-icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
